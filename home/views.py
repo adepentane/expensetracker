f45635a82d071fce08c903e0cb51b37d1cc9f3ff
adepentane <adepentane5@gmail.com>
2023-12-16 00:51:10 +0100

    moving to

--- home/views.py
+++ home/views.py
@@ -1,42 +1,25 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.shortcuts import render, HttpResponse, redirect
 from django.contrib import messages
-from django.contrib.auth import authenticate, logout
-from django.contrib.auth import login as dj_login
+from django.contrib.auth import authenticate, login as dj_login, logout
 from django.contrib.auth.models import User
 from .models import Addmoney_info, UserProfile
-from django.contrib.sessions.models import Session
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.db.models import Sum
-from django.http import JsonResponse
-import datetime
-from django.utils import timezone
+from django.core.paginator import Paginator
 
 
+def home(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+    return render(request, 'home/login.html')
 
-def home(request): 
-    if request.session.has_key('is_logged'):
-        return redirect('/index')
-    return render(request,'home/login.html')
 
-# return HttpResponse('This is home')
 def index(request):
-    if request.session.has_key('is_logged'):
-        user_id = request.session["user_id"]
-        user = User.objects.get(id=user_id)
+    if request.user.is_authenticated:
+        user = request.user
         addmoney_info = Addmoney_info.objects.filter(user=user).order_by('-Date')
-        paginator = Paginator(addmoney_info , 4)
+        paginator = Paginator(addmoney_info, 4)
         page_number = request.GET.get('page')
-        page_obj = Paginator.get_page(paginator,page_number)
-        context = {
-            # 'add_info' : addmoney_info,
-            'page_obj' : page_obj
-    }
-
-
-# if request.session.has_key('is_logged'):
+        page_obj = paginator.get_page(page_number)
+        context = {'page_obj': page_obj}
         return render(request, 'home/index.html', context)
     return redirect('home')
 
@@ -46,103 +29,63 @@ def addmoney(request):
 
 
 def profile(request):
-    if request.session.has_key('is_logged'):
+    if request.user.is_authenticated:
         return render(request, 'home/profile.html')
-    return redirect('/home')
+    return redirect('home')
 
 
 def profile_edit(request, id):
-    if request.session.has_key('is_logged'):
-        add = User.objects.get(id=id)
-        return render(request, 'home/profile_edit.html', {'add':add})
-    return redirect("/home")
+    if request.user.is_authenticated and request.user.id == id:
+        user = User.objects.get(id=id)
+        return render(request, 'home/profile_edit.html', {'user': user})
+    return redirect('home')
 
-# Updating Profiles
-def profile_update(request,id): 
-    if request.session.has_key('is_logged'):
+
+def profile_update(request, id):
+    if request.user.is_authenticated and request.user.id == id:
         if request.method == "POST":
             user = User.objects.get(id=id)
-            user.first_name = request.POST["fname"]
-            user.last_name = request.POST["lname"]
-            user.email = request.POST["email"]
-            user.userprofile.Savings = request.POST["Savings"]
-            user.userprofile.income = request.POST["income"]
-            user.userprofile.profession = request.POST["profession"]
-            user.userprofile.save()
+            user.first_name = request.POST.get("fname")
+            user.last_name = request.POST.get("lname")
+            user.email = request.POST.get("email")
+            profile = user.userprofile
+            profile.Savings = request.POST.get("Savings")
+            profile.income = request.POST.get("income")
+            profile.profession = request.POST.get("profession")
+            profile.save()
             user.save()
-            return redirect("/profile")
-    return redirect("/home")
+            return redirect('profile')
+    return redirect('home')
 
 
 def handleSignup(request):
-    if request.method =='POST':
-        # get the post parameters
+    if request.method == 'POST':
+        # handle signup logic
+        return redirect('login')
 
-        uname = request.POST["uname"]
-        fname=request.POST["fname"]
-        lname=request.POST["lname"]
-        email = request.POST["email"]
-        profession = request.POST['profession']
-        Savings = request.POST['Savings']
-        income = request.POST['income']
-        pass1 = request.POST["pass1"]
-        pass2 = request.POST["pass2"]
-        profile = UserProfile(Savings =
-        Savings,profession=profession, income=income)
-        # check for errors in input
-        if request.method == 'POST':
-            try:
-                user_exists = User.objects.get(username=request.POST['uname'])
-                messages.error(request, " Username already taken, Try something else!!!")
-                return redirect("/register")
-            except User.DoesNotExist:
-                if len(uname)>15:
-                    messages.error(request," Username must be max 15 characters, Please try again")
-                    return redirect("/register")
-        if not uname.isalnum():
-            messages.error(request," Username should only contain letters and numbers, Please try again")
-            return redirect("/register")
-        if pass1 != pass2:
-            messages.error(request," Password do not match, Please try again")
-            return redirect("/register")
-        # create the user
-        user = User.objects.create_user(uname, email, pass1)
-        user.first_name=fname
-        user.last_name=lname
-        user.email = email
-        # profile = UserProfile.objects.all()
-        user.save()
-        # p1=profile.save(commit=False)
-        profile.user = user
-        profile.save()
-        messages.success(request," Your account has been successfully created")
-        return redirect("/")
-    else:
-        return HttpResponse('404 - NOT FOUND ')
-    return redirect('/login')
+
 def handlelogin(request):
-    if request.method =='POST':
-        # get the post parameters
-        loginuname = request.POST["loginuname"]
-        loginpassword1=request.POST["loginpassword1"]
-        user = authenticate(username=loginuname, password=loginpassword1)
-        if user is not None:
-            dj_login(request, user)
-            request.session['is_logged'] = True
-            user = request.user.id
-            request.session["user_id"] = user
-            messages.success(request, " Successfully logged in")
-            return redirect('/index')
-        else:
-            messages.error(request," Invalid Credentials, Please try again")
-            return redirect("/")
-    return HttpResponse('404-not found')
+    if request.method == 'POST':
+        # handle login logic
+        return redirect('index')
 
 
 def handleLogout(request):
-    del request.session['is_logged']
-    del request.session["user_id"]
-    logout(request)
-    messages.success(request, " Successfully logged out")
+    if request.user.is_authenticated:
+        logout(request)
+        messages.success(request, "Successfully logged out")
     return redirect('home')
 
+
+def addmoney_submission(request):
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            user = request.user
+            add_money = request.POST.get("add_money")
+            quantity = request.POST.get("quantity")
+            Date = request.POST.get("Date")
+            Category = request.POST.get("Category")
+            add = Addmoney_info(user=user, add_money=add_money, quantity=quantity, Date=Date, Category=Category)
+            add.save()
+            return redirect('index')
+    return redirect('index')
