36391e144c639f45ba414c856878c9802ebb4f40
adepentane <adepentane5@gmail.com>
2023-12-23 11:16:38 +0100

    Added the other libraries

--- ExpenseTracker/settings.py
+++ ExpenseTracker/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l7-m5@wzb#cc*&h5eo@r%&s7(a9tp#&9vxu04lniyt4s^h2wfy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
