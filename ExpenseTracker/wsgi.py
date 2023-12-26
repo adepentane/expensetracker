cd3f5617fa7058b2d457d5fe5fcdc3c7cc88ffce
adepentane <adepentane5@gmail.com>
2023-12-26 23:05:55 +0100

    Reverted

--- ExpenseTracker/wsgi.py
+++ ExpenseTracker/wsgi.py
@@ -12,13 +12,9 @@ import sys
 
 from django.core.wsgi import get_wsgi_application
 
-# Add the directory containing the ExpenseTracker module to the Python path
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ExpenseTracker.deployment" if 'WEBSITE_HOSTNAME' in os.environ else "ExpenseTracker.settings")
-os.environ.setdefault('DJANGO_ALLOW_ASYNC_UNSAFE', 'true')
-sys.path.append('ExpenseTracker/')
 
-# settings_module = 'ExpenseTracker.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'ExpenseTracker.settings'
+settings_module = 'ExpenseTracker.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'ExpenseTracker.settings'
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
